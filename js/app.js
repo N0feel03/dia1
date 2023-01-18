0a8eb32b7805d1e6e13c5aa8c966f234f44f0512
Rodrigo Ramirez <Kejjaid6789@gmail.com>
2023-01-18 11:11:05 -0600

    Pagina terinada

--- js/app.js
+++ js/app.js
@@ -1,86 +1,69 @@
-console.log('conectado.....')
-
+console.log('conetado...')
 
 const productData = [
-    {
-
-    index:'01',
-    name:'yellow track',
-    Image: 'img1.jpg',
-    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
-
-
-},
 {
-
-    index:'02',
-    name:'designer oufit',
-    Image: 'img2.jpg',
-    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
-    
-    
+index: '01',
+name: 'Oufit One',
+Image: 'img1.jpg',
+des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
 }
 ,
 {
-
-    index:'03',
-    name:'chilling mood',
-    Image: 'img3.jpg',
-    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
-    
-    
-    }
-    
-
-
-
-
-
-
+index: '02',
+name: 'Oufit Two',
+Image: 'img2.jpg',
+des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
+}
+,
+{
+index: '03',
+name: 'Oufit Three',
+Image: 'img3.jpg',
+des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, aperiam, iusto nobis quidem quisquam vitae nemo doloribus libero aliquam fugit culpa molestias porro ad quis soluta ipsam sequi totam. Est.'
+}
 ]
 
-
 const nextBtn = document.querySelector('.nxt-btn')
+//console.log('Boton', nextBtn)
 
 let smImgContainer = document.querySelector('.sm-product-img-container')
-let smImg = document.querySelector('sm-product-img')
+let smImg = document.querySelector('.sm-product-img')
 let productIndex = document.querySelector('.product-index')
-let smProducDes = document.querySelector('.sm-product-des')
+let smProductDes = document.querySelector('.sm-product-des')
 let productImgContainer = document.querySelector('.product-img-container')
 let productImg = document.querySelector('.product-img')
 let backdropImg = document.querySelector('.backdrop-img')
-let productDetail = document.querySelector('.product-detail')
+let productDetail = document.querySelector('.detail-product')
 let productName = document.querySelector('.product-name')
 let productDes = document.querySelector('.product-des')
 
-
-
-
 let currentProduct = 0
 
+nextBtn.addEventListener('click', () => {
+    if(currentProduct >=  productData.length-1){
+        currentProduct = 0
+    }
+    else
+    {
+        currentProduct++
+    }
+    productIndex.innerHTML = productData[currentProduct].index
+    smProductDes.innerHTML = productData[currentProduct].des.slice(0, 80)
+    smImgContainer.classList.add('slide')
+    productImgContainer.classList.add('slide')
+    backdropImg.classList.add('fede')
+    productDetail.classList.add('fede')
 
+    setTimeout(() =>{
+        productName.innerHTML = productData[currentProduct].name
+        productDes.innerHTML = productData[currentProduct].des
+        smImg.src = productImg.src = backdropImg.src = `img/${productData[currentProduct].Image}`
+    }, 500)
 
-//console.log('Boton', nextBtn)
-
-
-nextBtn.addEventListener('click', () =>{
-
-//alert('hola')
-
-if(currentProduct >= productData.length -1){
-
-currentProduct=0
-
-}else{
-
-
-    currentProduct++
-}
-
-productIndex.innerHTML = productData[currentProduct].index
-smProductDes.innerHTML = productData[currentProduct].des.slice(0,80)
-smImgContainer.classList.add('slide')
-productImgContainer.classList.add('slide')
-backdropImg.classList.add('fade')
-productDetail.classList.add('fade')
+    setTimeout(() =>{
+    smImgContainer.classList.remove('slide')
+    productImgContainer.classList.remove('slide')
+    backdropImg.classList.remove('fede')
+    productDetail.classList.remove('fede')
+    }, 1000)
 })
